16986c3902d2c6f9b20a66e2a1be883031a303f9
Ovieh <ovieh.mosley@gmail.com>
2017-09-05 22:46:01 -0400

    Created objects to represents attackers & defenders.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,27 @@
 'use strict';
 
-function character(hp, ap, cap) {
+function character(name, hp, ap, cap) {
+    this.characterName = name;
     this.healthPoints = hp;
     this.attackPower = ap;
     this.counterAttackPower = cap;
 }
 
-var obi = new character(120, 15, 20);
-var luke = new character(100, 12, 25);
-var sidius = new character(150, 25, 10);
-var darth = new character(180, 23, 8);
+var obi = new character("Obi Wan-Kinobi", 120, 15, 20);
+var luke = new character("Luke Skywalker", 100, 12, 25);
+var sidius = new character("Darth Sidius", 150, 25, 10);
+var darth = new character("Darth Maul", 180, 23, 8);
+
+function Game() {
+    this.player = "";
+    this.defender = "";
+}
+
+var game = new Game();
+
+function battle(attacker, defender) {
+    console.log(attacker.attackPower);
+}
 
 $(function () {
     $(".player").one("click", function () {
@@ -18,10 +30,45 @@ $(function () {
         $(this).siblings().addClass("available-players").removeClass("player");
         $(this).appendTo("#player-choice");
         $("#picks").appendTo("#available");
+
+        switch ($(this).attr("id")) {
+            case "obi":
+                game.player = obi;
+                break;
+            case "luke":
+                game.player = luke;
+                break;
+            case "sidius":
+                game.player = sidius;
+                break;
+            case "darth":
+                game.player = darth;
+                break;
+        }
     });
 
     $(document).on("click", ".available-players", function () {
         $(this).addClass("defender").removeClass("available-players");
-        $(this).appendTo("#defender-area");     
+        $(this).appendTo("#defender-area");
+
+        switch ($(this).attr("id")) {
+            case "obi":
+                game.defender = obi;
+                break;
+            case "luke":
+                game.defender = luke;
+                break;
+            case "sidius":
+                game.defender = sidius;
+                break;
+            case "darth":
+                game.defender = darth;
+                break;
+        }
     });
+
+    $(document).on("click", "#attack", function () {
+        console.log(game.player.attackPower);
+    });
+
 });
