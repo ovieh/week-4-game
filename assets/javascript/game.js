f9a5656e1ccb282b2e81352d908de8d3a7bb91d7
Ovieh <ovieh.mosley@gmail.com>
2017-09-06 23:26:11 -0400

    Player attacks are displayed on screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,8 @@ function character(name, hp, ap, cap) {
 var obi = new character("Obi Wan-Kinobi", 120, 15, 20);
 var luke = new character("Luke Skywalker", 100, 12, 25);
 var sidius = new character("Darth Sidius", 150,  18, 10);
-var darth = new character("Darth Maul", 180, 18, 8);
+var maul = new character("Darth Maul", 180, 18, 8);
+var jar_jar = new character("Jar Jar Binks", 0, 1, 2);
 
 function Game() {
     this.player = "";
@@ -42,8 +43,8 @@ $(function () {
             case "sidius":
                 game.player = sidius;
                 break;
-            case "darth":
-                game.player = darth;
+            case "maul":
+                game.player = maul;
                 break;
         }
     });
@@ -62,22 +63,34 @@ $(function () {
             case "sidius":
                 game.defender = sidius;
                 break;
-            case "darth":
-                game.defender = darth;
+            case "maul":
+                game.defender = maul;
                 break;
         }
     });
 
     $(document).on("click", "#attack", function () {
         // console.log(game.player.attackPower);
+
+        if(game.defender.healthPoints <= 0 ) {
+            //drop defender and have player choose new foe.
+        }
+        else if (game.player.health <= 0){ 
+            
+        }
         game.defender.healthPoints -= game.player.attackPower*count;
-        game.player.healthPoints -= game.defender.attackPower;
+        $("#attacker").html("You attacked " + game.defender.characterName + " for " + game.player.attackPower*count);
+        
+        game.player.healthPoints -= game.defender.counterAttackPower;
+        $("#defender").html(game.defender.characterName + " attacked you for " + game.defender.counterAttackPower);
+        
         count++;
 
         $(".player h6.hp").html(game.player.healthPoints);
         $(".defender h6.hp").html(game.defender.healthPoints);
         // console.log(game.defender.attackPower*count);     
         
+
         
     });
 
